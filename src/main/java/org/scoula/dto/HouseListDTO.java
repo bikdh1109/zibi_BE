17363bf4a1787b58e54efd1475c8904a80cd00de
동욱 <du123kim@naver.com>
2025-07-31 09:50:59 +0900

    Fix : HouseList 고침

--- src/main/java/org/scoula/dto/HouseListDTO.java
+++ src/main/java/org/scoula/dto/HouseListDTO.java
@@ -15,10 +15,16 @@ public class HouseListDTO {
     private String pblancNo;
     @JsonProperty("application_period") // 접수 기간 문자열 (예: 2025.07.31 ~ 2025.08.05)
     private String applicationPeriod;
-//    @JsonProperty("suply_ar") // 공급 면적 (apt_type.suply_ar 또는 officetel_type.excluse_ar)
-//    private String suplyAr;
-//    @JsonProperty("lttot_top_amount") // 분양가 (apt_type.lttot_top_amount 또는 officetel_type.suply_amount)
-//    private String lttotTopAmount;
+    private String si;
+    private String sigungu;
     @JsonProperty("house_type") // 공고유형 (APT, 신혼희망타운, 오피스텔, 도시형생활주택)
     private String houseType;
+    @JsonProperty("min_area")
+    private String minArea;
+    @JsonProperty("max_area")
+    private String maxArea;
+    @JsonProperty("min_price")
+    private String minPrice;
+    @JsonProperty("max_price")
+    private String maxPrice;
 }
